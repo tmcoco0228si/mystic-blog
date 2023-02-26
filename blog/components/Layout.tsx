eeae08d2e7a10656c8881a98011c45961ad09a40
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-26 12:10:27 +0900

    Footer, Header, Layoutコンポーネント作成

---
+++ blog/components/Layout.tsx
@@ -0,0 +1,13 @@
+
+import Header from './Header';
+import Footer from './Footer';
+
+export default function Layout({ children }: any) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1 max-w-4xl w-full mx-auto">{children}</main>
+      <Footer />
+    </div>
+  );
+}
