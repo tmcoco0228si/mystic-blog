4cb9d31bad4bf039a1d6187684111b27b9783f72
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-26 23:40:42 +0900

    three.jsで作成したコンポーネント使用

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -1,20 +1,24 @@
 import { getPostsData } from "@/lib/posts";
-import PostCard from '../components/PostCard';
-
+import PostCard from "../components/PostCard";
+import ThreeObject from "@/components/ThreeObject";
 export async function getStaticProps() {
   const allPostsData = getPostsData();
   return {
     props: { allPostsData },
   };
-};
+}
 export default function Home({ allPostsData }: any) {
   return (
-    <div className="my-8 rounded-lg">
-    <div className="grid grid-cols-3 ">
-      {allPostsData.map((post: any) => (
-        <PostCard key={post.postId} post={post} />
-      ))}
-    </div>
-  </div>
+    <>
+    
+      <ThreeObject />
+      <div className="my-8 rounded-lg">
+        <div className="grid grid-cols-3 ">
+          {allPostsData.map((post: any) => (
+            <PostCard key={post.postId} post={post} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
