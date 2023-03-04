0ce681fe3be6a2cd35e7e69c12bee0b4b8486d78
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-03-04 21:29:12 +0900

    ゴーファーをマウスで動くように修正

--- components/ThreeObject.tsx
+++ components/ThreeObject.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const ThreeObject = () => {
   let canvas: HTMLElement;
-  let model: THREE.Group;
+  let obj: THREE.Group;
 
   useEffect(() => {
     if (canvas) return;
@@ -12,9 +13,10 @@ const ThreeObject = () => {
     canvas = document.getElementById("canvas")!;
     // シーン
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color("white");
     // サイズ
     const sizes = {
-      width: 200,
+      width: 800,
       height: 400,
     };
 
@@ -39,12 +41,13 @@ const ThreeObject = () => {
     const gltfLoader = new GLTFLoader();
     // gltfLoader.load("./models/shiba/shiba.gltf", (gltf) => {
     gltfLoader.load("./models/gopher/gopher.gltf", (gltf) => {
-      model = gltf.scene;
+      obj = gltf.scene;
       // オブジェクトのみの大きさ（余白等は対象外）
-      model.scale.set(0.75, 0.75, 0.75);
-      scene.add(model);
+      obj.scale.set(0.75, 0.75, 0.75);
+      scene.add(obj);
 
-      const pointLight = new THREE.PointLight(0xffffff, 1.25)
+      const pointLight = new THREE.AmbientLight(0xffffff, 1);
+      // const pointLight = new THREE.PointLight(0xffffff, 1.25)
       pointLight.position.set(1, 1, 100)
       camera.position.set(0, 2, 8);
       scene.add(pointLight);
@@ -52,9 +55,12 @@ const ThreeObject = () => {
       function animate() {
         requestAnimationFrame(animate);
         //横回転
-        model.rotation.y += 0.01;
+        obj.rotation.y += 0.01;
         renderer.render(scene, camera);
       }
+
+      new OrbitControls(camera, renderer.domElement);
+
       animate();
     });
   }, []);
