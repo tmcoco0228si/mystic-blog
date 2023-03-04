b228f7658ac80eaf27b89df423ea017b06d4f5bc
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-03-05 00:17:13 +0900

    _document.tsxを再作成 再作成をしてHTML要素全体にdark theme が適用されるように実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, useTheme } from "next-themes";
+import { createContext, useState } from "react";
 
+export const AllTheme = createContext<any>([]);
 export default function App({ Component, pageProps }: AppProps) {
+  const [objTheme, setObjTheme] = useState("");
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <AllTheme.Provider value={{ objTheme, setObjTheme }}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AllTheme.Provider>
+    </ThemeProvider>
   );
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+type Props = {}
+
+class Document extends NextDocument<Props> {
+  render() {
+    return (
+      <Html lang="ja-JP" className='dark'>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="format-detection" content="telephone=no" />
+
+        </Head>
+        <div className='dark:bg-darkgrey'>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+        </div>
+      </Html>
+    )
+  }
+}
+
+export default Document
