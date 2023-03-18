5bb54d6cdbf830c1c265ad4d78500202a122b3c3
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-03-18 15:41:52 +0900

    ハンバーガーメニューの対応

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,41 +1,105 @@
-import Link from "next/link";
-import { ChangeThemeButton } from "@/components/ChangeTheneButton";
+import React, { useState, useEffect, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  // Handle window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Close mobile menu when window size is larger than 1024px
+  useEffect(() => {
+    if (windowWidth >= 1024) {
+      setIsOpen(false);
+    }
+  }, [windowWidth]);
+
+  // Animation variants for the mobile menu
+  const menuVariants = {
+    open: { opacity: 1, scaleY: 1, originY: 0 },
+    closed: { opacity: 0, scaleY: 0, originY: 0 },
+  };
+
+  // Close mobile menu when clicked outside of the menu
+  const handleClickOutside = (event: MouseEvent) => {
+    if (event.target instanceof Element) {
+      if (!event.target.closest("header")) {
+        setIsOpen(false);
+      }
+    }
+  };
+  
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener("click", handleClickOutside);
+    } else {
+      window.removeEventListener("click", handleClickOutside);
+    }
+  
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen]);
+  
+
   return (
-    <header className="sticky top-0 border-b z-10 bg-white">
-      <div className="dark:bg-darkgrey">
-        <div className="max-w-4xl mx-auto flex justify-between items-center h-12">
-          <Link href="/">
-            <p className="">tmcoco0228si</p>
-          </Link>
-          <Link href="https://github.com/tmcoco0228si">
-            <div className="mt-2">
-              <button
-                type="button"
-                className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2 "
-              >
-                <svg
-                  className="w-4 h-4 mr-2 -ml-1 "
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="github"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 496 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-                  ></path>
-                </svg>
-                Github
-              </button>
-            </div>
-          </Link>
-          <ChangeThemeButton />
-        </div>
+    <header className="bg-blue-500 py-4">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <h1 className="text-white font-bold text-lg">MyLogo</h1>
+        <button
+          className="text-white lg:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          ≡
+        </button>
+        <AnimatePresence>
+  {isOpen && (
+    <motion.nav
+      className="z-10 absolute left-0 w-full mt-2 py-2 bg-white shadow-lg rounded lg:mt-0 lg:bg-transparent lg:shadow-none lg:w-auto lg:static lg:flex gap-4 text-black font-semibold lg:text-white"
+      initial="closed"
+      animate="open"
+      exit="closed"
+      variants={menuVariants}
+      transition={{ duration: 0.3 }}
+    >
+      <a href="/" className="block px-4 py-2 lg:hover:underline">
+        Home
+      </a>
+      <a href="/about" className="block px-4 py-2 lg:hover:underline">
+        About
+      </a>
+      <a
+        href="/contact"
+        className="block px-4 py-2 lg:hover:underline"
+      >
+        Contact
+      </a>
+    </motion.nav>
+  )}
+</AnimatePresence>
+        <nav className="hidden lg:flex gap-4 text-white">
+          <a href="/" className="hover:underline">
+            Home
+          </a>
+          <a href="/about" className="hover:underline">
+            About
+          </a>
+          <a href="/contact" className="hover:underline">
+            Contact
+          </a>
+        </nav>
       </div>
     </header>
   );
