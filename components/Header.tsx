38881f9d9ae4f4f1141d0305db90e3d300f7f8ac
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-03-19 10:54:05 +0900

    3Dモデルの大きさを修正することで、スマホサイズ時の余白削除対応完了

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,12 +53,10 @@ const Header = () => {
   return (
     <header className="w-full bg-gray-800 text-white shadow-md fixed top-0 left-0 z-10">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex justify-start items-center">
+    <div className={`container ${windowWidth >= 640 ? 'mx-auto px-4' : ''} sm:px-6 lg:px-8 flex justify-start items-center`}>
       <h1 className="text-white font-bold text-xs tiny:text-sm sm:text-lg md:text-xl mr-auto">
         Mystic blog
       </h1>
-
-
 
         <button
           className="text-white lg:hidden"
